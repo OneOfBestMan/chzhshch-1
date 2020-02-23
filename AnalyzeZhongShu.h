519dbbca141e3cb5ffa9aa3b6af951a75e3d269d
xlzh502 <xlzh502@outlook.com>
2020-02-23 15:40:30 +0800

    ver 63

---
+++ AnalyzeZhongShu.h
@@ -0,0 +1,114 @@
+#ifndef ANALYZEZHONGSHU_H
+#define ANALYZEZHONGSHU_H
+
+
+
+
+template <class XianDuanClass>
+class AnalyzeZhongShu_Template
+{
+public:
+	typedef typename XianDuanClass::baseItemType_Container::iterator baseItemIterator;
+	typedef typename XianDuanClass::baseItemType baseItemType;
+	typedef typename XianDuanClass::ContainerType ContainerType;
+	typedef typename XianDuanClass::baseItemType_Container baseItemType_Container;
+
+
+	void handleTurningPoint()
+	{
+/*
+
+  对于向上的线段说， TurningPoint(TP)是指 这样的点： TP1、TP2
+
+               TP2       /
+                /\      /
+     TP1       /  \    /
+      /\      /    \  /
+     /  \    /      \/
+    /    \  /
+   /      \/
+  /
+ 
+ 对于向下的线段，TurningPoint是指 这样的点：TP1、TP2
+  \
+   \        /\
+    \      /  \
+	 \    /    \        /\
+	  \  /      \      /  \
+	   \/        \    /    \
+      TP1         \  /      \
+                   \/        \
+                  TP2         \
+                               \
+*/
+
+
+	}
+
+	void handleJuxtaposition()
+	{
+/*
+  对于向上的线段说， Juxtaposition(JP)是指 这样的相邻（且有重合区域的）线段: JP0/JP1/JP2/JP3 、  JP2/JP3/JP4/JP5
+
+                           /\
+                          /  \      /
+                         /   JP5   /
+                /\     JP4     \  /
+               /  \    /        \/
+      /\     JP2  JP3 /
+     /  \    /      \/
+   JP0  JP1 /
+   /      \/
+  /
+
+  对于向下的线段说， Juxtaposition是指 这样的相邻（且有重合区域的）线段: JP0/JP1/JP2/JP3 、  JP2/JP3/JP4/JP5
+
+  \
+   \      /\
+   JP0  JP1 \          
+	 \  /    \        /\
+      \/    JP2     JP3 \
+               \    /    \          /
+                \  /      \        /
+                 \/       JP4    JP5
+                            \    /
+                             \  /
+                              \/
+
+  对于下列的线段，Juxtaposition是指： JP0/JP1/JP2/JP3/JP4/JP5
+   
+                                 /\
+                                /  \
+                               /    \
+                /\            /      \
+     /\        /  \          /        \
+    /  \      /    \        /          \
+  JP0  JP1  JP2    JP3    JP4          JP5
+  /      \  /        \    /              \
+          \/          \  /                \
+                       \/
+*/
+
+	}
+
+	void doWork() 
+	{
+		handleTurningPoint();
+		handleJuxtaposition();
+	}
+};
+
+
+
+template <class XianDuanClass>
+AnalyzeZhongShu_PostOrder()
+{
+	typedef typename XianDuanClass::baseItemType baseItemType;
+
+	AnalyzeZhongShu_PostOrder<baseItemType>();
+	AnalyzeZhongShu_Template<XianDuanClass>();
+}
+
+
+
+#endif
--- Class_ZhongShu.h
+++ Class_ZhongShu.h
@@ -6,10 +6,21 @@
 #include "traits.h"
 #include "Class_XianDuan.h"
 
-template <int grade>
-class Class_ZhongShu: public traits<typename Class_XianDuan<grade-1>, typename Class_ZhongShu<grade>>
+
+class IZhongShu
 {
 public:
+	virtual int getGrade() = 0;
+};
+
+
+template <int grade>
+class Class_ZhongShu: public traits<typename Class_ZhongShu<grade-1>, typename Class_ZhongShu<grade>>
+{
+public:
+
+	static const int GRADE = grade;
+
 	Class_ZhongShu(void);
 	~Class_ZhongShu(void);
 
@@ -52,8 +63,13 @@ public:
                                   此处低点一定要低于Low
 */
 
-	/* 第三买、卖点： 次级别走势会拉（三卖）或会跌（三买），不触及中枢区间，则该次级别走势的终点是第三买卖点； 并且要求，该次级别的起点，应该高于中枢波动高点（三买）或低于中枢波动低点（三卖） */
-	baseItemType thirdPoint;
+	/* 第三买、卖点： 次级别走势回拉（三卖）或回跌（三买），不触及中枢区间，则该次级别走势的终点是第三买卖点； 并且要求，该次级别的起点，应该高于中枢波动高点（三买）或低于中枢波动低点（三卖） */
+
+	/*
+	getStart() 返回中枢起始的那一笔
+	getEnd() 返回中枢回拉、回踩的那一笔，第三买、卖点就是这一笔的末端点；
+	*/
+
 };
 
 
@@ -67,6 +83,13 @@ template<int grade>
 Class_ZhongShu<grade>::~Class_ZhongShu(void)
 {
 }
+
+
+template<>
+class Class_ZhongShu<1>: public traits<typename Class_XianDuan<1>, typename Class_ZhongShu<1>>
+{
+};
+
 
 
 
